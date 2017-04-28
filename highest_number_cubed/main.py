4fa266179b184f15518c2f62ce97366716c2835e
Matt Martinez <mattm1823@gmail.com>
2017-04-28 01:45:02 +0000

    We did it reddit!

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,12 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    count = 0
+    cubed_number = 0
+    while cubed_number <= limit:
+        cubed_number = count**3
+        if cubed_number >  limit:
+            return count - 1
+        else:
+            count = count + 1
+        
